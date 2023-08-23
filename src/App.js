98be851c23602c94b87ed805af5dec39bd3a5ecf
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-08-23 23:22:21 +0200

    Add quotes api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quotes from './components/Qoutes';
+
+// end imports
 
 function App() {
   return (
     <div className="App">
 
-      <div className="Calculator">
+      <div className="Container">
         <Calculator />
+        <Quotes />
       </div>
     </div>
   );
---
+++ src/components/Qoutes.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import $ from 'jquery';
+import './ButtonStyles.css';
+
+// end imports
+
+function Quotes() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = () => {
+      const category = 'learning';
+
+      $.ajax({
+        method: 'GET',
+        url: `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+        headers: {
+          'X-Api-Key': 'kP1kTQjEYKr4mhstUEZLXw==VUr0CLQqm9FJBQhX',
+          'Content-Type': 'application/json',
+        },
+        contentType: 'application/json',
+        success(result) {
+          setData(result);
+          setLoading(false);
+        },
+        error: function ajaxError(jqXHR) {
+          setError(new Error(jqXHR.responseText));
+          setLoading(false);
+        },
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div className="Quote">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="Quote"><h4>😐Oops Something went wrong. Please refresh the Page. If the Error Continues Contact our support.</h4></div>;
+  }
+
+  return (
+    <div className="Quote">
+      <h1>Quote</h1>
+      <ul>
+        {data.map((quote, index) => (
+          <li key={index}>
+            <i>{quote.quote}</i>
+            <br />
+            <b>{quote.author}</b>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Quotes;
